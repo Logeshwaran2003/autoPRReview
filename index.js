da32b19c7eadf5fb060abc2c1d804bcf514ed84d
Logeshwaran2003 <93189506+Logeshwaran2003@users.noreply.github.com>
2025-03-04 00:46:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 function arrangeCircles(sheetWidth, sheetHeight, circleDiameter) {
-    let positions = [];
-    let row = 0;
-    let col = 0;
-    let totalCircles = 0;
-    
-    while (row * circleDiameter < sheetHeight) {
-
