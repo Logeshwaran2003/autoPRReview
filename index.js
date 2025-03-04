f5950f3e340dbd2e146f0f67131df4decc4524eb
Logeshwaran2003 <93189506+Logeshwaran2003@users.noreply.github.com>
2025-03-04 11:51:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function arrangeCircles(sheetWidth, sheetHeight, circleDiameter) {
     const circlesPerRow = Math.floor(cricPerrow);
     const circlesPerColumn = Math.floor(sheetHeight / circleDiameter);
     const totalCircles = circlesPerRow 
-////
+//
